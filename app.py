c06079f4c66931c704f814d5219849c870722c1d
Rahul Singh <contact@rahools.com>
2020-02-20 01:33:46 +0530

    added index.

--- app.py
+++ app.py
@@ -8,8 +8,14 @@ app.config["DEBUG"] = True
 @app.route('/issue', methods=['GET'])
 def issueFunc():
     with open('issue.pkl', 'rb') as f:
-        issue =  = pickle.load(f)
+        issue = pickle.load(f)
+
     return issue[random.choice(range(len(issue)))]
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello, GRE takers. Visit /issue or /argument'
+
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
